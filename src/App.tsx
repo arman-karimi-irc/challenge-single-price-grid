1d78344b0296905d5f831aeada4f87de1c6aad69
iirccopilot <iirccopilot@gmail.com>
2026-01-07 13:59:15 +0330

    style: implement join us

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Subscription from "./Subscription";
 import WhyUs from "./WhyUs";
 function App() {
   return (
-    <div className="bg-teal-100 h-screen w-screen ">
-      <div className="flex flex-col shadow-xl shadow-gray-500/20">
+    <div className="bg-teal-100 h-screen w-screen flex items-center justify-center px-4">
+      <div className="flex flex-col bg-white shadow-xl shadow-gray-500/20 rounded-md px-6 py-6">
         <JoinUs />
         <Subscription />
         <WhyUs />
--- src/JoinUs.tsx
+++ src/JoinUs.tsx
@@ -1,9 +1,17 @@
 export default function JoinUs() {
   return (
-    <div className="">
-      <h2 className="">Join our community</h2>
-      <h3 className="">30-day, hassle-free money back guarantee</h3>
-      <p className="">Gain access to our full library of tutorials along with expert code</p>
+    <div className="flex flex-col space-y-5 mb-6">
+      <h2 className="text-teal-500 font-semibold text-xl">
+        Join our community
+      </h2>
+      <h3 className="text-green-400 font-semibold">
+        30-day, hassle-free money back guarantee
+      </h3>
+      <p className="text-gray-500 leading-7">
+        Gain access to our full library of tutorials along with expert code
+        reviews. Perfect for any developers who are serious about honing their
+        skills.
+      </p>
     </div>
   );
 }
