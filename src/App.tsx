0afd0a7156f8cb77288d458132fe2ccab0b71738
iirccopilot <iirccopilot@gmail.com>
2026-01-07 13:34:27 +0330

    basic componenet structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import JoinUs from "./JoinUs";
+import Subscription from "./Subscription";
+import WhyUs from "./WhyUs";
 function App() {
   return (
-    <>
-    </>
-  )
+    <div className="bg-teal-100 h-screen w-screen ">
+      <div className="flex flex-col">
+        <JoinUs />
+        <Subscription />
+        <WhyUs />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
